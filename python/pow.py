670371054c931e46a0ca23efc94337a0823adde8
toona-laptop <amanou.toona@gmail.com>
2022-08-25 22:04:22 +0900

    Add: mod pow

---
+++ python/pow.py
@@ -0,0 +1,24 @@
+def pow(x, y, mod=None):
+
+    if y == -1:
+        y = mod - 2
+
+    res = 1
+
+    xx = x
+    while y > 0:
+
+        if y & 1:
+            res *= xx
+
+        if mod:
+            res %= mod
+
+        y >>= 1
+
+        xx *= xx
+    return res
+
+
+if __name__ == "__main__":
+    print(pow(2, 10, 3))
