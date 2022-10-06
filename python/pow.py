e7e3c5896b6e821bb7a9c09ce18eec3c59358e85
toona <amanou.toona@gmail.com>
2022-10-06 22:33:57 +0900

    faster pow with mod

--- python/pow.py
+++ python/pow.py
@@ -4,19 +4,20 @@ def pow(x, y, mod=None):
         y = mod - 2
 
     res = 1
-
     xx = x
     while y > 0:
 
         if y & 1:
             res *= xx
-
         if mod:
             res %= mod
 
         y >>= 1
 
         xx *= xx
+        if mod:
+            xx %= mod
+
     return res
 
 
